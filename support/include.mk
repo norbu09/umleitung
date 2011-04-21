5b5496c6a00510eafc16ad4ea568baf39372e23f
Tobias Kirschstein <lev@ideegeo.com>
2011-04-21 16:02:25 +1200

    - ignore apps in ebin/ - don't try to build documentation for now as it crashes

--- support/include.mk
+++ support/include.mk
@@ -25,7 +25,8 @@ EMULATOR := beam
 ERL_SOURCES := $(wildcard *.erl)
 ERL_HEADERS := $(wildcard *.hrl) $(wildcard ../include/*.hrl)
 ERL_OBJECTS := $(ERL_SOURCES:%.erl=$(EBIN_DIR)/%.$(EMULATOR))
-ERL_DOCUMENTS := $(ERL_SOURCES:%.erl=$(DOC_DIR)/%.html)
+#ERL_DOCUMENTS := $(ERL_SOURCES:%.erl=$(DOC_DIR)/%.html)
+ERL_DOCUMENTS := 
 ERL_OBJECTS_LOCAL := $(ERL_SOURCES:%.erl=./%.$(EMULATOR))
 APP_FILES := $(wildcard *.app)
 EBIN_FILES = $(ERL_OBJECTS) $(ERL_DOCUMENTS) $(APP_FILES:%.app=../ebin/%.app)
